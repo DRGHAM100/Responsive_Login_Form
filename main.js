72ac54ba76507fe1bacd9ffce68357086c15a8e1
Drgham Zaref <53182741+DRGHAM100@users.noreply.github.com>
2020-04-10 21:43:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,21 @@ $(function(){
     $('input[type=text]').focus(function(){
         $('.input-div.one').addClass('focus');
     }).blur(function(){
-        $('.input-div.one').removeClass('focus');
+       
+        if( $('input[type=text]').val() == ''){
+            $('.input-div.one').removeClass('focus');
+        }
+        
     });
 
     $('input[type=password]').focus(function(){
         $('.input-div.two').addClass('focus');
     }).blur(function(){
-        $('.input-div.two').removeClass('focus');
+       
+        if( $('input[type=password]').val() == ''){
+            $('.input-div.two').removeClass('focus');
+        }
+        
     });
 
 
